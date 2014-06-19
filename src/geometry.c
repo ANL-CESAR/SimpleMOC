eee2155f235fcc95603ac0698a307cadb963519c
geoff <geogunow@geoffpc.(none)>
2014-06-19 13:13:43 -0500

    Adding geometry logic in 2D. Needs to be extended to 3D cylindrical geometry.

--- src/geometry.c
+++ src/geometry.c
@@ -1,18 +1,54 @@
 #include"SimpleMOC_header.h"
 
 // Finds the "localized" region ID of a 3D cartesian coordinate
-RegionID get_region_id( double x, double y, double z, Input input, Reactor reactor )
+RegionID get_region_id( double x, double y, double z, Input input, Reactor reactor, double * radii )
 {
 	RegionID id;
 
 	// find assembly
-	id.assembly = 0;
+	int x_assembly_id = (int) x / reactor.assembly_width;
+	int y_assembly_id = (int) y / reactor.assembly_width;
+	id.assembly = x_assembly_id + input.x_assemblies * y_assembly_id;
 	
 	// find pin cell
 	id.pin = 0;
+
+	// calculate the (x,y) coordinates in the assembly
+	double assembly_x = (double) x - x_assembly_id * reactor.assembly_width;
+	double assembly_y = (double) y - y_assembly_id * reactor.assembly_width;
+
+	// find pin cell in the assembly (assuming 17 x 17 assemblies)
+    int x_pin_id = (int) assembly_x / reactor.pin_cell_width;
+	int y_pin_id = (int) assembly_y / reactor.pin_cell_width;
+	id.pin = x_pin_id + 17 * y_pin_id;
+
+	// TODO: Add Z component (3D)
+
+	// calculate the (x,y) coordinates in the pin
+	double pin_x = (double) assembly_x - x_pin_id * reactor.pin_cell_width;
+	pin_x -= reactor.pin_cell_width/2; // take x coordinate from center of pin
+	double pin_y = (double) assembly_y - y_pin_id * reactor.pin_cell_width;
+	pin_y -= reactor.pin_cell_width/2; // take y coordinate from center of pin
+	double radius = sqrt( pin_x * pin_x + pin_y * pin_y );
+
+	// find the ring inside pin cell
+	int ring_id = 0;
+    for(int i=0; i < reactor.n_radial_regions; i++)
+	{
+		if(radius < reactor.n_radial_regions)
+			ring_id++;
+		else
+			break;
+	}
+
+	// find the azimuthal region within the ring
+	double theta = atan2(pin_y, pin_x);
+	double pi = 3.14159265358979323846264338327950;
+	double azimuthal_interval = 2 * pi / reactor.n_azimuthal_regions;
+	int azimuthal_id = (int) theta / azimuthal_interval;
 	
-	// find the zone inside pin cell
-	id.zone = 0;
+	// compute zone id
+	id.zone = azimuthal_id + reactor.n_azimuthal_regions * ring_id;
 
 	return id;
 }
@@ -20,7 +56,16 @@ RegionID get_region_id( double x, double y, double z, Input input, Reactor react
 // Finds the serialized "global" index of a region 
 long get_region_index( RegionID id, Input input, Reactor reactor )
 {
-	return 0;
+	// calculate number of zones in a pin cell (add 1 for outside region)
+	zones_per_pin = (reactor.n_radial_regions + 1) * reactor.n_azimuthal_regions;
+
+	// define number of pins per assembly (assuming 17 x 17 assemblies)
+	pins_per_assembly = 289;
+
+	// TODO: Add Z component (3D)
+
+	// calculate the index number and return
+	return id.zone + zones_per_pin * ( id.pin + pins_per_assembly * id.assembly)
 }
 
 // Calculates constant volume radii
