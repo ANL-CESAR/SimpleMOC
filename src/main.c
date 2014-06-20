a4cee0ca20e02307061f00879fc868d561563e94
John Tramm <john.tramm@gmail.com>
2014-06-20 09:03:01 -0400

    added a few temporary tests to main

--- src/main.c
+++ src/main.c
@@ -8,6 +8,15 @@ int main( int argc, char * argv[] )
 	Reactor reactor = reactor_init();
 	Input input = get_input();
 
+	// RegionID test
+	RegionID id = get_region_id( 1.23, 54.32, 4.9, input, reactor);	
+	printf("Region ID:\n\tAssembly: %d\n\tPin Cell: %d\n\tZone: %d\n",
+			id.assembly, id.pin, id.zone);
+
+	// Serialized ID test
+	long index = get_region_index( id, input, reactor );
+	printf("Serialized Index: %ld\n", index);
+
 
 	return 0;
 }
