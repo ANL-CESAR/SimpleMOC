22c31c0cb1dd91c1190e946ef68c8f6834c809b8
John Tramm <john.tramm@gmail.com>
2014-07-22 18:22:49 -0500

    small tweaks

--- src/main.c
+++ src/main.c
@@ -40,11 +40,9 @@ int main( int argc, char * argv[] )
 		#endif
 		renormalize_flux(params,input, grid);                // MPI Global Accumulate
 		res = update_sources(params, input, keff);     // Local
-		#ifdef MPI
 		keff = compute_keff(params, input, grid);            // MPI Global Accumulate
 		if( mype == 0 )
 			printf("keff = %lf\n", keff);
-		#endif
 	}
 
 	free_2D_tracks( params.tracks_2D );
