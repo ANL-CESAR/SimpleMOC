c3f26a8db0e1dee33ebf2152bf33cb28777325c6
John Tramm <john.tramm@gmail.com>
2014-08-01 15:36:46 -0500

    tweaked some PAPI settings

--- src/papi.c
+++ src/papi.c
@@ -23,7 +23,7 @@ void counter_init( int *eventset, int *num_papi_events, Input I )
 	{
 		*num_papi_events = 2;
 		events = (int *) malloc( *num_papi_events * sizeof(int));
-		events[0] = PAPI_DP_OPS;
+		events[0] = PAPI_FP_OPS;
 		events[1] = PAPI_TOT_CYC;
 	}
 	// Bandwidth
@@ -406,7 +406,7 @@ void counter_stop( int * eventset, int num_papi_events, Input I )
 				LLC_cache_miss += values[i];
 			if( strcmp(info.symbol, "PAPI_TOT_CYC") == 0 )
 				total_cycles += values[i];
-			if( strcmp(info.symbol, "PAPI_DP_OPS") == 0 )
+			if( strcmp(info.symbol, "PAPI_FP_OPS") == 0 )
 				FLOPS += values[i];
 			if( strcmp(info.symbol, "RESOURCE_STALLS:ANY") == 0 )
 				stall_any += values[i];
