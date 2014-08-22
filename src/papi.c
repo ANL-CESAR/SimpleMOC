17dd118f57e9d5b375a1d9b41bab7f745442a110
Shuo He <shuohe@ftlogin1.mcp.alcf.anl.gov>
2014-08-22 19:46:53 +0000

    moved thread local free(events) to end of function

--- src/papi.c
+++ src/papi.c
@@ -455,7 +455,6 @@ void counter_stop( int * eventset, int num_papi_events, Input * I )
             }
         }
         free( I->vals_accum );
-		free(events);
 
 		border_print();
 		center_print("PERFORMANCE SUMMARY", 79);
@@ -488,6 +487,7 @@ void counter_stop( int * eventset, int num_papi_events, Input * I )
 
 		border_print();
 	}
+    free(events);
 }
 
 #endif
